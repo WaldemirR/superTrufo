d0a97abdab4ad56f3a3fb3206ead84ca04ab78f6
Waldemir Rodrigues <waldemir.rodrigues88@gmail.com>
2022-09-18 22:23:42 -0300

    atualização dos link's dos jpgadores e a inclusão da validação dos pontos e mais uma condicional no javascript para valida jogador escolhido x jogador máquina

---
+++ javascript.js
@@ -0,0 +1,496 @@
+var carta1 = {
+    nome: "Vitinho",
+    imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Vitinho.jpg/250px-Vitinho.jpg",
+    atributos: {
+        ataque: 9,
+        contraAtaque: 7,
+        defesa: 5,
+    }
+};
+var carta2 = {
+    nome: "Casemiro",
+    imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/20180610_FIFA_Friendly_Match_Austria_vs._Brazil_Casemiro_850_1575.jpg/800px-20180610_FIFA_Friendly_Match_Austria_vs._Brazil_Casemiro_850_1575.jpg",
+    atributos: {
+        ataque: 8,
+        contraAtaque: 08,
+        defesa: 3,
+
+    }
+};
+var carta3 = {
+    nome: "Lendário Tafarel",
+    imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Taffarel_2018.png/250px-Taffarel_2018.png",
+    atributos: {
+        ataque: 4,
+        contraAtaque: 5,
+        defesa: 10,
+
+    }
+};
+var carta4 = {
+    nome: "Ronaldinho Gaúcho",
+    imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Ronaldinho_Ga%C3%BAcho_em_junho_de_2019.jpg/250px-Ronaldinho_Ga%C3%BAcho_em_junho_de_2019.jpg",
+    atributos: {
+        ataque: 10,
+        contraAtaque: 10,
+        defesa: 6,
+
+    }
+};
+var carta5 = {
+    nome: "Lendário Diego Maradona",
+    imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Maradona-Mundial_86_con_la_copa.JPG/240px-Maradona-Mundial_86_con_la_copa.JPG",
+    atributos: {
+        ataque: 10,
+        contraAtaque: 9,
+        defesa: 5,
+
+    }
+};
+var carta6 = {
+    nome: "Mbáppe",
+    imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/2019-07-17_SG_Dynamo_Dresden_vs._Paris_Saint-Germain_by_Sandro_Halank%E2%80%93088.jpg/250px-2019-07-17_SG_Dynamo_Dresden_vs._Paris_Saint-Germain_by_Sandro_Halank%E2%80%93088.jpg",
+    atributos: {
+        ataque: 9,
+        contraAtaque: 9,
+        defesa: 3,
+
+    }
+};
+var carta7 = {
+    nome: "Curtois",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4y2ePuBYAQiLaq8fZJoW3tGqA5tic75XatQ&usqp=CAU",
+    atributos: {
+        ataque: 5,
+        contraAtaque: 5,
+        defesa: 10,
+
+    }
+};
+var carta8 = {
+    nome: "Richarlison Pombo",
+    imagem: "https://upload.wikimedia.org/wikipedia/commons/5/5b/07_07_2019_Final_da_Copa_Am%C3%A9rica_2019_%2848226557731%29_%28cropped%29.jpg",
+    atributos: {
+        ataque: 10,
+        contraAtaque: 8,
+        defesa: 4,
+
+    }
+};
+var carta9 = {
+    nome: "Rames Rodriquez",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIRfdPKxbNCbFy_c2tYZbalHdCPq6kslbDAA&usqp=CAU",
+    atributos: {
+        ataque: 9,
+        contraAtaque: 06,
+        defesa: 5,
+
+    }
+};
+var carta10 = {
+    nome: "Balotelli",
+    imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Mario_Balotelli_OM_%28cropped%29.jpg/230px-Mario_Balotelli_OM_%28cropped%29.jpg",
+    atributos: {
+        ataque: 9,
+        contraAtaque: 9,
+        defesa: 3,
+
+    }
+};
+var carta11 = {
+    nome: "Andrea Pirlo",
+    imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/1_andrea_pirlo_2014.jpg/250px-1_andrea_pirlo_2014.jpg",
+    atributos: {
+        ataque: 10,
+        contraAtaque: 10,
+        defesa: 5,
+
+    }
+};
+var carta12 = {
+    nome: "Suárez",
+    imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Luis_Su%C3%A1rez_Atl%C3%A9tico_Madrid.jpg/230px-Luis_Su%C3%A1rez_Atl%C3%A9tico_Madrid.jpg",
+    atributos: {
+        ataque: 10,
+        contraAtaque: 5,
+        defesa: 6,
+
+    }
+};
+var carta13 = {
+    nome: "Lendário Ronaldinho Fenômeno",
+    imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/051119SMcC0014.jpg/220px-051119SMcC0014.jpg",
+    atributos: {
+        ataque: 10,
+        contraAtaque: 10,
+        defesa: 7,
+
+    }
+};
+var carta14 = {
+    nome: "Karín Benzema",
+    imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Karim_Benzema_wearing_Real_Madrid_home_kit_2021-2022.jpg/250px-Karim_Benzema_wearing_Real_Madrid_home_kit_2021-2022.jpg",
+    atributos: {
+        ataque: 10,
+        contraAtaque: 07,
+        defesa: 12,
+
+    }
+};
+var carta15 = {
+    nome: "Pedro",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRDyXB1q6plWQLyFWJmFQ26DZNBfOHzVsTfA&usqp=CAU",
+    atributos: {
+        ataque: 10,
+        contraAtaque: 6,
+        defesa: 5,
+
+    }
+};
+var carta16 = {
+    nome: "Zinedine Zidane",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQy0XK8QgvFs5VKQTtksl_O2UZq5Dd8XKo-Bw&usqp=CAU",
+    atributos: {
+        ataque: 10,
+        contraAtaque: 10,
+        defesa: 2,
+
+    }
+};
+var carta17 = {
+    nome: "Gerrard",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPDaYyCUyEivd27vK5UEl4mP_d_7-bbvW8gw&usqp=CAU",
+    atributos: {
+        ataque: 9,
+        contraAtaque: 9,
+        defesa: 2,
+
+    }
+};
+var carta18 = {
+    nome: "Lendário Rei Pelé",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqXv7G77d0IdU3L1yWWeZO5_z_JBlG9Oi-3A&usqp=CAU",
+    atributos: {
+        ataque: 10,
+        contraAtaque: 10,
+        defesa: 8,
+
+    }
+};
+var carta19 = {
+    nome: "Lendário Rivaldo",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTV2QvcH8uf34lL1E-_E8eFziimY5eDj8_Ysg&usqp=CAU",
+    atributos: {
+        ataque: 10,
+        contraAtaque: 07,
+        defesa: 12,
+
+    }
+};
+var carta20 = {
+    nome: "Lendária Rainha Marta",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCQCx-xYtEfwscz9B1NPVCCj9CcdKtZ5EoIw&usqp=CAU",
+    atributos: {
+        ataque: 10,
+        contraAtaque: 10,
+        defesa: 8,
+
+    }
+};
+var carta21 = {
+    nome: "David Rooney",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfc-rtqw6PGKI1jYGO5jeFcN-gpV3BLVjuIw&usqp=CAU",
+    atributos: {
+        ataque: 9,
+        contraAtaque: 9,
+        defesa: 2,
+
+    }
+};
+var carta22 = {
+    nome: "Gerrard Pique",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxhu6DthljWVGyQ19MiAp_JR8FDNaTNHcyQw&usqp=CAU",
+    atributos: {
+        ataque: 5,
+        contraAtaque: 6,
+        defesa: 10,
+
+    }
+};
+var carta23 = {
+    nome: "Sergio Busquets",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxzJT8bJqdpeS8y-9mpgFO0nZowsy-Uoi_Cw&usqp=CAU",
+    atributos: {
+        ataque: 6,
+        contraAtaque: 6,
+        defesa: 7,
+
+    }
+};
+var carta24 = {
+    nome: "Cristiano Ronaldo",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO5dnefOm0-JIXHO81r5Qtj6uh_4Oqk2aXzA&usqp=CAU",
+    atributos: {
+        ataque: 10,
+        contraAtaque: 10,
+        defesa: 7,
+
+    }
+};
+var carta25 = {
+    nome: "Thierry Henry",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfflhiQB_BZtRAMwopALiBHAYOcaLOoDDY_w&usqp=CAU",
+    atributos: {
+        ataque: 10,
+        contraAtaque: 9,
+        defesa: 7,
+
+    }
+};
+var carta26 = {
+    nome: "Raheem Sterling",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsu79N_GkF27Rsfhygopm-H8nBrCMBlv3ToA&usqp=CAU",
+    atributos: {
+        ataque: 10,
+        contraAtaque: 9,
+        defesa: 2,
+
+    }
+};
+var carta27 = {
+    nome: "Robert Lewandowski",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSIEtFj_SoDha8yEuOgbnmJL2zNNTolCbSSg&usqp=CAU",
+    atributos: {
+        ataque: 10,
+        contraAtaque: 07,
+        defesa: 12,
+
+    }
+};
+var carta28 = {
+    nome: "Sadio Mané",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyBqXuu6VB8jtNRxhHbsz2vSJBumjvqTgZRA&usqp=CAU",
+    atributos: {
+        ataque: 10,
+        contraAtaque: 08,
+        defesa: 2,
+
+    }
+};
+var carta29 = {
+    nome: "Mohamed Salah",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlqcZhr8vGc2WFCsbWZuDJSJX6n1AKT-XAKw&usqp=CAU",
+    atributos: {
+        ataque: 10,
+        contraAtaque: 10,
+        defesa: 7,
+
+    }
+};
+var carta30 = {
+    nome: "Ederson",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSu8VuTpHdQj1qKkHo0IT0H8CHxq38n4Nr9Wg&usqp=CAU",
+    atributos: {
+        ataque: 6,
+        contraAtaque: 8,
+        defesa: 10,
+
+    }
+};
+var carta31 = {
+    nome: "Marcos",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcQ8VwL0OeaXBPD9eg8LuTkxYHGVg3qh9inw&usqp=CAU",
+    atributos: {
+        ataque: 7,
+        contraAtaque: 7,
+        defesa: 9,
+
+    }
+};
+var carta32 = {
+    nome: "Luigi Buffon",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuRtITywTm9uCq3ik7gcb9nIjDd01B4VhD9A&usqp=CAU",
+    atributos: {
+        ataque: 7,
+        contraAtaque: 7,
+        defesa: 10,
+
+    }
+};
+var carta33 = {
+    nome: "De Gea",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyGjsd5xWdhxOoFFo3Th8PVrc-LIZGyceB6Q&usqp=CAU",
+    atributos: {
+        ataque: 3,
+        contraAtaque: 3,
+        defesa: 7,
+
+    }
+};
+var carta34 = {
+    nome: "Ilker Cassilias",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZhbZT0G_mthMXED1HZFSxWXWCdE73V8GlcA&usqp=CAU",
+    atributos: {
+        ataque: 7,
+        contraAtaque: 7,
+        defesa: 9,
+
+    }
+};
+var carta35 = {
+    nome: "Dida",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa6-W5ZtfhJShQuC_0BtDkTtisbHgXSiWTzQ&usqp=CAU",
+    atributos: {
+        ataque: 8,
+        contraAtaque: 8,
+        defesa: 10,
+    }
+};
+var carta36 = {
+    nome: "Claudio Bravo",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3d03fNtMHzDVVxlTWd8HqTGy87_NKVjmNBg&usqp=CAU",
+    atributos: {
+        ataque: 3,
+        contraAtaque: 2,
+        defesa: 7,
+
+    }
+};
+var carta37 = {
+    nome: "Navas",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWd4sJXa42IVNEG4QYGxeZUwQ4GlrfvBUd6g&usqp=CAU",
+    atributos: {
+        ataque: 10,
+        contraAtaque: 07,
+        defesa: 12,
+
+    }
+};
+var carta38 = {
+    nome: "Alex-Morgana",
+    imagem: "https://melhorde10.com/wp-content/uploads/2021/03/Alex-Morgan-jogadora-de-futebol.jpg",
+    atributos: {
+        ataque: 10,
+        contraAtaque: 10,
+        defesa: 8,
+
+    }
+};
+var carta39 = {
+    nome: "Ada Hegerberg",
+    imagem: "https://www.esportelandia.com.br/wp-content/uploads/2019/08/ada-herberg-maior-artilheira-uma-edicao-liga-dos-campeoes-feminina-768x542.jpeg",
+    atributos: {
+        ataque: 10,
+        contraAtaque: 10,
+        defesa: 9,
+
+    }
+};
+var carta40 = {
+    nome: "Wendie Renard",
+    imagem: "https://www.esportelandia.com.br/wp-content/uploads/2020/03/wendie-renard.jpeg",
+    atributos: {
+        ataque: 10,
+        contraAtaque: 07,
+        defesa: 12,
+
+    }
+};
+
+var cartas = [carta1, carta2,carta3, carta4, carta6, carta7, carta8, carta9, carta10, carta11, carta12, carta13, carta14, carta16, carta17, carta20, carta21, carta22, carta23, carta24, carta25, carta26, carta27, carta28, carta29, carta30, carta31, carta32, carta33, carta34, carta35, carta36, carta37, carta38, carta39, carta40];
+var cartaMaquina;
+var cartaJogador;
+
+function sortearCarta() {
+    var numeroCartaMaquina = parseInt(Math.random() * 40);
+    cartaMaquina = cartas[numeroCartaMaquina];
+
+    var numeroCartaJogador = parseInt(Math.random() * 40);
+    while (numeroCartaMaquina == numeroCartaJogador) {
+        numeroCartaJogador = parseInt(Math.random() * 40);
+    }
+    cartaJogador = cartas[numeroCartaJogador];
+    console.log(cartaJogador);
+
+    document.getElementById("btnSortear").disabled = true
+    document.getElementById("btnJogar").disabled = false
+    
+    exibirCartaJogador();
+}
+
+function obtemAtributoSelecionado() {
+    var radioAtributo = document.getElementsByName("atributo");
+
+    for (var i = 0; i < radioAtributo.length; i++) {
+        if (radioAtributo[i].checked == true) {
+            return radioAtributo[i].value;
+        }
+    }
+}
+
+function jogar() {
+
+    var atributoSelecionado = obtemAtributoSelecionado();
+
+    var divResultado = document.getElementById("resultado");
+    
+
+    if (
+        cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
+
+        htmlResultado = "<p class='resultado-final'>Mandou muito bem,você venceu!!</p>";
+
+    } else if (
+        cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado] ) {
+
+        htmlResultado = "<p class='resultado-final'>Não foi dessa vez,você perdeu</p>"
+    } else {
+
+        htmlResultado = "<p class='resultado-final'>Empatou, que tal melhor de 5 ?</p>"
+    }
+    
+    divResultado.innerHTML = htmlResultado
+    document.getElementById('btnJogar').disabled = true;
+    exibirCartaMaquina()
+   
+}
+
+function exibirCartaJogador() {
+    var divCartaJogador = document.getElementById("carta-jogador");
+    divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
+    // Chamar função de outro modo que traga o mesmo resultado da forma mais atualizada
+    // divCartaJogador.style.backgroundImage = url("+ cartaJogador.imagem + ")"
+
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+    var tagHTML = "<div id='opcoes' class= 'carta-status'>";
+
+    var opcoesTexto = "";
+    for (var atributo in cartaJogador.atributos) {
+        opcoesTexto +="<input type='radio'  name = 'atributo' value=''" + atributo + "'>" + atributo + " " + cartaJogador.atributos[atributo] + "<br>";
+    }
+    var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`;
+
+    divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
+
+
+function exibirCartaMaquina() {
+    var divCartaMaquina = document.getElementById("carta-maquina");
+    divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
+    // Chamar função de outro modo que traga o mesmo resultado da forma mais atualizada
+    // divCartaJogador.style.backgroundImage = "url("+ cartaJogador.imagem + ")"
+
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+    var tagHTML = "<div id='opcoes' class = 'carta-status'>";
+
+    var opcoesTexto = "";
+    for (var atributo in cartaMaquina.atributos) {
+        opcoesTexto +=
+            opcoesTexto = "<p type='text'  name = 'atributo' value=''" + atributo + "'>" + atributo + " " + cartaMaquina.atributos[atributo] + "<br>";
+    }
+    var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`;
+
+    divCartaMaquina.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+
+
+}
